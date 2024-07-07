7288307e36cca4a06fa03807ab7c5a6d55815697
Carlos <62247499+baArlene@users.noreply.github.com>
2024-07-07 16:03:51 +0200

    Search query using pathname

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,23 +1,21 @@
-import { fetchUsers } from '@/app/lib/data'
-import Pagination from '@/app/ui/dashboard/pagination/Pagination'
-import Search from '@/app/ui/dashboard/search/Search'
-import styles from '@/app/ui/dashboard/users/users.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
+import { fetchUsers } from "@/app/lib/data";
+import Pagination from "@/app/ui/dashboard/pagination/Pagination";
+import Search from "@/app/ui/dashboard/search/Search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
+const UsersPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
 
-const UsersPage = async () => {
-
-  const users = await fetchUsers();
-
-  console.log(users)
+  const users = await fetchUsers(q);
 
   return (
     <div className={styles.containter}>
       <div className={styles.top}>
         <Search placeholder="Search for a user..." />
-        <Link href='/dashboard/users/add'>
-        <button className={styles.addButton}>Add New User</button>
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New User</button>
         </Link>
       </div>
       <table className={styles.table}>
@@ -32,31 +30,43 @@ const UsersPage = async () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) =>(
-          <tr key={user.id}>
-            <td>
-              <div className={styles.user}><Image src={user.img ||'/noavatar.png'} alt='' width={40} height={40} className={styles.userImage} />
-              {user.username}</div>
-            </td>
-            <td>{user.email}</td>
-            <td>{user.createdAt?.toString().slice(4,16)}</td>
-            <td>{user.isAdmin ? "Admin" : "Client"}</td>
-            <td>{user.isActive ? "Active" : "Passive"}</td>
-            <td>
-              <div className={styles.buttons}>
-              <Link href={`/dashboard/users/${user.id}`}>
-                <button className={`${styles.button} ${styles.view}`}>View</button>
-              </Link>
-              <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-              </div>
-            </td>
-          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "Passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
       <Pagination />
     </div>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
--- app/ui/dashboard/search/Search.jsx
+++ app/ui/dashboard/search/Search.jsx
@@ -1,13 +1,40 @@
-import { MdSearch } from 'react-icons/md'
-import styles from './search.module.css'
+"use client";
 
-const Search = ({placeholder}) => {
+import { MdSearch } from "react-icons/md";
+import styles from "./search.module.css";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
+const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  {
+    /* useDebouncedCallback is used to prevent the search from being triggered too frequently that is to wait for user to finish typing */
+  }
+  const handleSearch = useDebouncedCallback((e) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (e.target.value) {
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className={styles.container}>
-        <MdSearch />
-        <input type="text" placeholder={placeholder} className={styles.input} />
+      <MdSearch />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
